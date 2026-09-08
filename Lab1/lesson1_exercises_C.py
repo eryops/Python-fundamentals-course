96f2f718f077167f2082276c320a737bda1cd95f
Johanna <majliiz@hotmail.com>
2026-09-08 14:44:05 +0200

    exercise 3 done

---
+++ Lab1/lesson1_exercises_C.py
@@ -0,0 +1,35 @@
+# full_sentence = "    The quick brown fox jumps over the lazy dog.  "
+# print(len(full_sentence))
+# print(full_sentence.upper())
+# print(full_sentence.lower())
+# print(full_sentence.strip())
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+
+# print(f"{first_name.strip().capitalize()} {last_name.strip().capitalize()}")
+
+# string1 = "python programming"
+
+# print(string1[:1])  # prints the first character
+# print(string1[-1:])  # prints the last character
+# print(string1[0:6])  # prints the first 6 characters
+# print(string1[-11:]) # prints the last 11 characters
+# print(string1[::-1]) # prints the string in reverse order
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+
+# username = first_name.strip().lower()[:3] + last_name.strip().lower()[:5]
+# print(f"Your username is: {username}")
+
+# email = input("Enter your email address: ")
+# name = email.split("@")[0]
+# domain = email.split("@")[1]
+
+# print(f"Name: {name}")
+# print(f"Domain: {domain}")
+
+# message = "Hello, Java World!"
+# new_message = message.replace("Java", "Python")
+# print(f"Original message: {message} and the new message is: {new_message}")
