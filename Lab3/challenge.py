31b2899be7b712d3994bd81d941d380718bef456
Johanna <majliiz@hotmail.com>
2026-09-11 15:19:16 +0200

    lab 3 challenge part 4

--- Lab3/challenge.py
+++ Lab3/challenge.py
@@ -14,7 +14,7 @@ flights = [
         'destination': 'Landvetter',
         'departure_time': '12:30',
         'gate': '4C',
-        'passengers': 99,
+        'passengers': 134,
         'maximum_capacity': 167,
         'delay_in_minutes': 0,
         'is_cancelled': False,
@@ -24,7 +24,7 @@ flights = [
         'destination': 'Oslo',
         'departure_time': '14:40',
         'gate': '87',
-        'passengers': 99,
+        'passengers': 65,
         'maximum_capacity': 167,
         'delay_in_minutes': 0,
         'is_cancelled': False,
@@ -44,7 +44,7 @@ flights = [
         'destination': 'Gran Canaria',
         'departure_time': '14:00',
         'gate': '89',
-        'passengers': 99,
+        'passengers': 134,
         'maximum_capacity': 167,
         'delay_in_minutes': 0,
         'is_cancelled': False,
@@ -54,7 +54,7 @@ flights = [
         'destination': 'New York',
         'departure_time': '19:00',
         'gate': '2',
-        'passengers': 99,
+        'passengers': 167,
         'maximum_capacity': 167,
         'delay_in_minutes': 1,
         'is_cancelled': False,
@@ -64,7 +64,7 @@ flights = [
         'destination': 'London',
         'departure_time': '23:50',
         'gate': '1B',
-        'passengers': 99,
+        'passengers': 44,
         'maximum_capacity': 167,
         'delay_in_minutes': 59,
         'is_cancelled': False,
@@ -74,7 +74,7 @@ flights = [
         'destination': 'Helsinki',
         'departure_time': '16:10',
         'gate': '66',
-        'passengers': 99,
+        'passengers': 166,
         'maximum_capacity': 167,
         'delay_in_minutes': 20,
         'is_cancelled': False,
@@ -84,7 +84,7 @@ flights = [
         'destination': 'Amsterdam',
         'departure_time': '14:45',
         'gate': '4D',
-        'passengers': 99,
+        'passengers': 22,
         'maximum_capacity': 167,
         'delay_in_minutes': 19,
         'is_cancelled': False,
@@ -94,17 +94,35 @@ flights = [
         'destination': 'Berlin',
         'departure_time': '21:00',
         'gate': '5A',
-        'passengers': 99,
+        'passengers': 150,
         'maximum_capacity': 167,
         'delay_in_minutes': 60,
         'is_cancelled': True,
     },
 ]
 
+number_of_scheduled_flights = 0
+number_of_canceled_flights = 0
+number_of_delayed_flights = 0
+number_of_flights_on_time = 0
+total_number_of_passengers = 0
+largest_number_of_passengers = 0
+number_of_flight_with_80_percent_filled = 0
+
 for flight in flights:
+    number_of_scheduled_flights += 1
+    total_number_of_passengers += flight['passengers']
+
+    if flight['passengers'] > largest_number_of_passengers:
+        largest_number_of_passengers = flight['passengers']
+
+    if flight['passengers'] / flight['maximum_capacity'] > 0.8:
+        number_of_flight_with_80_percent_filled += 1
+
     status = 'ON TIME'
     if flight['is_cancelled']:
         status = 'CANCELLED'
+        number_of_canceled_flights += 1
     elif flight['delay_in_minutes'] > 59:
         status = 'SEVERELY DELAYED'
     elif flight['delay_in_minutes'] > 19:
@@ -112,4 +130,20 @@ for flight in flights:
     elif flight['delay_in_minutes'] > 0:
         status = 'SLIGHT DELAY'
 
+    if status == 'ON TIME':
+        number_of_flights_on_time += 1
+    elif status != 'CANCELLED':
+        number_of_delayed_flights += 1    
+
     print(f"{flight['flight_number']} to {flight['destination']} leaving from Gate {flight['gate']} at {flight['departure_time']} is {status}")
+
+print(f"""
+number_of_scheduled_flights {number_of_scheduled_flights}
+number_of_canceled_flights {number_of_canceled_flights}
+number_of_delayed_flights {number_of_delayed_flights}
+number_of_flights_on_time  {number_of_flights_on_time}
+total_number_of_passengers {total_number_of_passengers}
+average_number_of_passengers {total_number_of_passengers/number_of_scheduled_flights}
+largest_number_of_passengers {largest_number_of_passengers}
+number_of_flight_with_80_percent_filled {number_of_flight_with_80_percent_filled}
+""")
