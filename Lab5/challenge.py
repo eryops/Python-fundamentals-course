1928bd379533b3948a877f3875118d2c34aa7682
Johanna <majliiz@hotmail.com>
2026-09-21 14:51:07 +0200

    lab 5 challange

--- Lab5/challenge.py
+++ Lab5/challenge.py
@@ -17,9 +17,9 @@ customers = [
     {"name": "Sara Lind", "email": "sara@example.com", "id": "C005"},
 ]
 
-def create_order(orderID, customer, *products, **options):
+def create_order(order_id, customer, *products, **options):
     return{
-        'order_id': orderID,
+        'order_id': order_id,
         'customer': customer,
         'products': list(products),
         'options': options
@@ -27,10 +27,10 @@ def create_order(orderID, customer, *products, **options):
 
 
 order1 = create_order(22, customers[1], 'Headphones', 'Backpack', shipping_method = 'pick up', shipping = None)
-order2 = create_order(22, customers[0], 'Notebook', 'Keyboard', shipping_method = 'pick up')
-order1 = create_order(22, customers[1], 'Notebook', 'Backpack', 'Water Bottle', shipping_method = 'post', campaign_code = 'free-delivery')
-order1 = create_order(22, customers[1], 'Keyboard', 'Notebook', 'Backpack', shipping_method = 'post', priority = 'next_day')
-order1 = create_order(22, customers[1], 'Notebook', 'Backpack', shipping_method = 'pick up')
+order2 = create_order(23, customers[0], 'Notebook', 'Keyboard', shipping_method = 'pick up')
+order3 = create_order(24, customers[1], 'Notebook', 'Backpack', 'Water Bottle', shipping_method = 'post', campaign_code = 'free-delivery')
+order4 = create_order(25, customers[1], 'Keyboard', 'Notebook', 'Backpack', shipping_method = 'post', priority = 'next_day')
+order5 = create_order(26, customers[1], 'Notebook', 'Backpack', shipping_method = 'pick up')
 
 
 def calculate_subtotal(*prices): 
@@ -39,4 +39,16 @@ def calculate_subtotal(*prices):
 def order_configuration(**options):
     return { key: value for key, value in options.items() if value is not None }
 
-# order_options = order_configuration(**order1['options'])
+def order_summary(order_id, customer, *messages, **metadata):
+    summery = f'Order Summary for {customer['name']} order {order_id}\nEmail: {customer['email']}\n'
+    if messages:
+        summery += 'Messages:\n'
+        for message in messages:
+            summery += f'{message}\n'
+    if metadata:
+        summery += 'Metadata\n'
+        for key, value in metadata.items():
+            summery += f'{key}: {value}\n'
+
+    return summery
+print(order_summary(22, customers[0], "snyggt kör", 'bra pris', shipping = 33, discount = 5))
