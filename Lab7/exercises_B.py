f9a9633ce7065c0007ef51f8bd9b2d437619cb42
Johanna <majliiz@hotmail.com>
2026-09-22 16:39:26 +0200

    lag 7 task B

--- Lab7/exercises_B.py
+++ Lab7/exercises_B.py
@@ -16,9 +16,31 @@ class BankAccount:
     def __repr__(self):
         return f'Bank account (owner = {self.owner}, balance = {self.balance})'
 
-account1 = BankAccount('Johanna', 1000)
-print(account1)
-account1.deposit(200)
-print(account1)
-account1.withdraw(2000)
+# account1 = BankAccount('Johanna', 1000)
+# print(account1)
+# account1.deposit(200)
+# print(account1)
+# account1.withdraw(2000)
+
+class Task:
+    def __init__(self, title, completed=False):
+        self.title = title
+        self.completed = completed
+
+    def complete(self):
+        self.completed = True
+
+    def reopen(self):
+        self.completed = False
+
+    def __repr__(self):
+        return f'Task(title={self.title}, completed={self.completed})'
+
+task_one = Task('Clean dishes')
+task_two = Task('Clean bathroom')
+
+task_one.complete()
+print(task_one)
+print(task_two)
+        
         
