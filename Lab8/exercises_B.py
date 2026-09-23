33d9cb74af686043a3e97de2446c37a2e1c46446
Johanna <majliiz@hotmail.com>
2026-09-23 14:45:26 +0200

    lab 8 part b

---
+++ Lab8/exercises_B.py
@@ -0,0 +1,17 @@
+movie_dictionary = {
+    'title': 'A title',
+    'director': 'A director',
+    'rating': 3.2
+}
+
+class Movie:
+    def __init__(self, title, director, rating):
+        self.title = title
+        self.director = director
+        self.rating = rating
+
+    def is_highly_rated(self):
+        return self.rating >= 8
+
+movie = Movie('Movie name', 'NN', 8.7)
+print(movie.is_highly_rated())  
