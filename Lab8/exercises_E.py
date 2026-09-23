fb488c0c338ca048dc7427937c56541c57ead2c7
Johanna <majliiz@hotmail.com>
2026-09-23 16:03:49 +0200

    lab 8 part E

---
+++ Lab8/exercises_E.py
@@ -0,0 +1,20 @@
+class Device:
+    def __init__(self, brand, year):
+        self.brand = brand.strip().title()
+        self.year = int(year)
+
+class Laptop(Device):
+    def __init__(self, brand, year, ram_gb):
+        super().__init__(brand, year)
+        self.ram_gb = ram_gb
+
+    def __repr__(self):
+            return f'Laptop {self.brand}, year {self.year}, ram {self.ram_gb}GB'
+
+class Phone(Device):
+    def __init__(self, brand, year, model):
+        super().__init__(brand, year)
+        self.model = model
+
+    def __repr__(self):
+        return f'Phone {self.brand}, year {self.year}, model {self.model}'
